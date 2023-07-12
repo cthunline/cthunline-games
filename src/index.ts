7ff55128ab3e31429577bac1a2eaf5d4a34c5445
Lord Grosse Jeanine <yoyojeanine@gmail.com>
2023-07-12 23:36:24 +0200

    fixed index exports

--- src/index.ts
+++ src/index.ts
@@ -10,21 +10,22 @@ import seventhSeaDefault from './seventhSea/default';
 import { swd6CharacterSchema } from './starWarsD6/schema';
 import swd6Default from './starWarsD6/default';
 
-export default {
-    callOfCthulhu: {
-        schema: cocCharacterSchema,
-        default: cocDefault
-    },
-    dnd5: {
-        schema: dnd5CharacterSchema,
-        default: dnd5Default
-    },
-    seventhSea: {
-        schema: seventhSeaCharacterSchema,
-        default: seventhSeaDefault
-    },
-    starWarsD6: {
-        schema: swd6CharacterSchema,
-        default: swd6Default
-    }
+export const callOfCthulhu = {
+    schema: cocCharacterSchema,
+    default: cocDefault
+};
+
+export const dnd5 = {
+    schema: dnd5CharacterSchema,
+    default: dnd5Default
+};
+
+export const seventhSea = {
+    schema: seventhSeaCharacterSchema,
+    default: seventhSeaDefault
+};
+
+export const starWarsD6 = {
+    schema: swd6CharacterSchema,
+    default: swd6Default
 };
