b52ae9aa63c49f8c859d1a118844468d4a87b8c0
Lord Grosse Jeanine <yoyojeanine@gmail.com>
2025-02-16 11:28:37 +0100

    added game id variable and type + reorganized index

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
+import type { TSchema } from '@sinclair/typebox';
+
 import { alienDefault } from './alien/default.js';
 import { alienCharacterSchema } from './alien/schema.js';
+import type { AlienCharacter } from './alien/types.js';
 
 import { apocalypseWorldData } from './apocalypseWorld/data.js';
 import { apocalypseWorldDefault } from './apocalypseWorld/default.js';
 import { apocalypseWorldCharacterSchema } from './apocalypseWorld/schema.js';
+import type { ApocalypseWorldCharacter } from './apocalypseWorld/types.js';
 
 import { cocDefault } from './callOfCthulhu/default.js';
 import { cocCharacterSchema } from './callOfCthulhu/schema.js';
+import type { CoCCharacter } from './callOfCthulhu/types.js';
 
 import { dnd5Default } from './dnd5/default.js';
 import { dnd5CharacterSchema } from './dnd5/schema.js';
+import type { DnD5Character } from './dnd5/types.js';
 
 import { seventhSeaDefault } from './seventhSea/default.js';
 import { seventhSeaCharacterSchema } from './seventhSea/schema.js';
+import type { SeventhSeaCharacter } from './seventhSea/types.js';
 
 import { swd6Default } from './starWarsD6/default.js';
 import { swd6CharacterSchema } from './starWarsD6/schema.js';
+import type { SWD6Character } from './starWarsD6/types.js';
 
 import { warhammerFantasyData } from './warhammerFantasy/data.js';
 import { warhammerFantasyDefault } from './warhammerFantasy/default.js';
 import { warhammerFantasyCharacterSchema } from './warhammerFantasy/schema.js';
-
-import type { AlienCharacter } from './alien/types.js';
-import type { ApocalypseWorldCharacter } from './apocalypseWorld/types.js';
-import type { CoCCharacter } from './callOfCthulhu/types.js';
-import type { DnD5Character } from './dnd5/types.js';
-import type { SeventhSeaCharacter } from './seventhSea/types.js';
-import type { SWD6Character } from './starWarsD6/types.js';
 import type { WarhammerFantasyCharacter } from './warhammerFantasy/types.js';
 
 export * from './alien/types.js';
@@ -37,6 +38,25 @@ export * from './seventhSea/types.js';
 export * from './starWarsD6/types.js';
 export * from './warhammerFantasy/types.js';
 
+export type GameId =
+    | 'alien'
+    | 'apocalypseWorld'
+    | 'callOfCthulhu'
+    | 'dnd5'
+    | 'seventhSea'
+    | 'starWarsD6'
+    | 'warhammerFantasy';
+
+export const gameIds = [
+    'alien',
+    'apocalypseWorld',
+    'callOfCthulhu',
+    'dnd5',
+    'seventhSea',
+    'starWarsD6',
+    'warhammerFantasy'
+] as const satisfies GameId[];
+
 export type Character =
     | AlienCharacter
     | ApocalypseWorldCharacter
@@ -46,39 +66,50 @@ export type Character =
     | SWD6Character
     | WarhammerFantasyCharacter;
 
-export const alien = {
-    schema: alienCharacterSchema,
-    default: alienDefault
-};
+const gamesData = {
+    alien: {
+        schema: alienCharacterSchema,
+        default: alienDefault
+    },
+    apocalypseWorld: {
+        schema: apocalypseWorldCharacterSchema,
+        default: apocalypseWorldDefault,
+        data: apocalypseWorldData
+    },
+    callOfCthulhu: {
+        schema: cocCharacterSchema,
+        default: cocDefault
+    },
+    dnd5: {
+        schema: dnd5CharacterSchema,
+        default: dnd5Default
+    },
+    seventhSea: {
+        schema: seventhSeaCharacterSchema,
+        default: seventhSeaDefault
+    },
+    starWarsD6: {
+        schema: swd6CharacterSchema,
+        default: swd6Default
+    },
+    warhammerFantasy: {
+        schema: warhammerFantasyCharacterSchema,
+        default: warhammerFantasyDefault,
+        data: warhammerFantasyData
+    }
+} as const satisfies Record<
+    GameId,
+    {
+        schema: TSchema;
+        default: object;
+        data?: object;
+    }
+>;
 
-export const apocalypseWorld = {
-    schema: apocalypseWorldCharacterSchema,
-    default: apocalypseWorldDefault,
-    data: apocalypseWorldData
-};
-
-export const callOfCthulhu = {
-    schema: cocCharacterSchema,
-    default: cocDefault
-};
-
-export const dnd5 = {
-    schema: dnd5CharacterSchema,
-    default: dnd5Default
-};
-
-export const seventhSea = {
-    schema: seventhSeaCharacterSchema,
-    default: seventhSeaDefault
-};
-
-export const starWarsD6 = {
-    schema: swd6CharacterSchema,
-    default: swd6Default
-};
-
-export const warhammerFantasy = {
-    schema: warhammerFantasyCharacterSchema,
-    default: warhammerFantasyDefault,
-    data: warhammerFantasyData
-};
+export const alien = gamesData.alien;
+export const apocalypseWorld = gamesData.apocalypseWorld;
+export const callOfCthulhu = gamesData.callOfCthulhu;
+export const dnd5 = gamesData.dnd5;
+export const seventhSea = gamesData.seventhSea;
+export const starWarsD6 = gamesData.starWarsD6;
+export const warhammerFantasy = gamesData.warhammerFantasy;
